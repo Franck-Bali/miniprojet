2c9b45eb59647117735f88b917a126abaa3bfce9
NickyDev <nicky121086@gmail.com>
2021-02-02 15:10:07 +0300

    Signed-off-by: Franck-Bali

---
+++ Windows/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.NameValider = new System.Windows.Forms.Button();
+            this.NameMdp = new System.Windows.Forms.TextBox();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.NameMail = new System.Windows.Forms.TextBox();
+            this.NameMontant = new System.Windows.Forms.TextBox();
+            this.NamePrenom = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.NameNom = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(6)))), ((int)(((byte)(42)))));
+            this.panel1.Controls.Add(this.NameValider);
+            this.panel1.Controls.Add(this.NameMdp);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.NameMail);
+            this.panel1.Controls.Add(this.NameMontant);
+            this.panel1.Controls.Add(this.NamePrenom);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.NameNom);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // NameValider
+            // 
+            this.NameValider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.NameValider.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.NameValider.FlatAppearance.BorderSize = 0;
+            this.NameValider.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NameValider.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.NameValider.ForeColor = System.Drawing.Color.White;
+            this.NameValider.Location = new System.Drawing.Point(173, 359);
+            this.NameValider.Name = "NameValider";
+            this.NameValider.Size = new System.Drawing.Size(138, 42);
+            this.NameValider.TabIndex = 5;
+            this.NameValider.Text = "Valider";
+            this.NameValider.UseVisualStyleBackColor = false;
+            // 
+            // NameMdp
+            // 
+            this.NameMdp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(6)))), ((int)(((byte)(42)))));
+            this.NameMdp.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.NameMdp.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NameMdp.ForeColor = System.Drawing.Color.White;
+            this.NameMdp.Location = new System.Drawing.Point(152, 309);
+            this.NameMdp.Name = "NameMdp";
+            this.NameMdp.PasswordChar = '*';
+            this.NameMdp.Size = new System.Drawing.Size(206, 22);
+            this.NameMdp.TabIndex = 3;
+            this.NameMdp.TextChanged += new System.EventHandler(this.NameNom_TextChanged);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.White;
+            this.panel6.Location = new System.Drawing.Point(152, 337);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(206, 1);
+            this.panel6.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Location = new System.Drawing.Point(152, 287);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(206, 1);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(152, 222);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(206, 1);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(152, 180);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(206, 1);
+            this.panel3.TabIndex = 4;
+            // 
+            // NameMail
+            // 
+            this.NameMail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(6)))), ((int)(((byte)(42)))));
+            this.NameMail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.NameMail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NameMail.ForeColor = System.Drawing.Color.White;
+            this.NameMail.Location = new System.Drawing.Point(152, 259);
+            this.NameMail.Name = "NameMail";
+            this.NameMail.Size = new System.Drawing.Size(206, 22);
+            this.NameMail.TabIndex = 3;
+            this.NameMail.TextChanged += new System.EventHandler(this.NameNom_TextChanged);
+            // 
+            // NameMontant
+            // 
+            this.NameMontant.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(6)))), ((int)(((byte)(42)))));
+            this.NameMontant.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.NameMontant.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NameMontant.ForeColor = System.Drawing.Color.White;
+            this.NameMontant.Location = new System.Drawing.Point(152, 194);
+            this.NameMontant.Name = "NameMontant";
+            this.NameMontant.Size = new System.Drawing.Size(206, 22);
+            this.NameMontant.TabIndex = 3;
+            this.NameMontant.TextChanged += new System.EventHandler(this.NameNom_TextChanged);
+            // 
+            // NamePrenom
+            // 
+            this.NamePrenom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(6)))), ((int)(((byte)(42)))));
+            this.NamePrenom.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.NamePrenom.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NamePrenom.ForeColor = System.Drawing.Color.White;
+            this.NamePrenom.Location = new System.Drawing.Point(152, 152);
+            this.NamePrenom.Name = "NamePrenom";
+            this.NamePrenom.Size = new System.Drawing.Size(206, 22);
+            this.NamePrenom.TabIndex = 3;
+            this.NamePrenom.TextChanged += new System.EventHandler(this.NameNom_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(152, 132);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(206, 1);
+            this.panel2.TabIndex = 4;
+            // 
+            // NameNom
+            // 
+            this.NameNom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(6)))), ((int)(((byte)(42)))));
+            this.NameNom.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.NameNom.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NameNom.ForeColor = System.Drawing.Color.White;
+            this.NameNom.Location = new System.Drawing.Point(152, 104);
+            this.NameNom.Name = "NameNom";
+            this.NameNom.Size = new System.Drawing.Size(206, 22);
+            this.NameNom.TabIndex = 3;
+            this.NameNom.TextChanged += new System.EventHandler(this.NameNom_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(37, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 21);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Montant :";
+            this.label6.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(37, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 21);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Mot de passe :";
+            this.label5.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(37, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 21);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Mail :";
+            this.label4.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(37, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Prénom :";
+            this.label3.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(37, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nom :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(527, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(519, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(233, 245);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox NameNom;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox NameMail;
+        private System.Windows.Forms.TextBox NameMontant;
+        private System.Windows.Forms.TextBox NamePrenom;
+        private System.Windows.Forms.TextBox NameMdp;
+        private System.Windows.Forms.Button NameValider;
+    }
+}
+
